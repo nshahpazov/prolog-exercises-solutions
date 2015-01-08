3ee55884398df4f8db46d40e258c29a63c292635
Nikola Shahpazov <nshahpazov@gmail.com>
2015-01-08 17:34:57 +0200

    rotate a list

---
+++ lists/rotate.pl
@@ -0,0 +1,6 @@
+% rotate a list to the left (uses split procedure)
+% rotate([1,2,3,4,5], -2, R).
+% R = [4, 5, 1, 2, 3].
+rotate([], _, []).
+rotate(L, N, R) :- length(L, N2), N1 is N2 + N,
+  append(Y, X, R), split(L, N1, X, Y).
