f4e0d4d03720d7bde11ae2ee9cbe6ff130ce0775
Nikola Shahpazov <nshahpazov@gmail.com>
2015-01-08 17:36:07 +0200

    init

---
+++ lists/drop.pl
@@ -0,0 +1,10 @@
+% drop nth element
+% ?- drop([a, b, c, d, e, f, g, h, i, k], 3, X).
+% X = [a, b, d, e, g, h, k]
+
+drop_([], _, _, R).
+drop_([H|T], N, K, [H|R]) :- K mod N =\= 0, K1 is K + 1,
+  drop_(T, N, K1, R).
+drop_([_|T], N, K, R) :- K mod N =:= 0, K1 is K + 1,
+  drop_(T, N, K1, R).
+drop(L, N, R) :- drop_(L, N, 1, R).
