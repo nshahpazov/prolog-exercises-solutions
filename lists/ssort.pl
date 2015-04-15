2205865a687366a1e730b2802dff574647a646bf
nshahpazov <nshahpazov@gmail.com>
2015-04-15 21:28:04 +0300

    selection sort

--- lists/ssort.pl
+++ lists/ssort.pl
@@ -2,19 +2,18 @@
 list_min([L|Ls], Min) :-
     list_min(Ls, L, Min).
 
+% private procedure with argument passing
 list_min([], Min, Min).
 list_min([H|Ls], Min0, Min) :-
     Min1 is min(H, Min0), list_min(Ls, Min1, Min).
 
-% remove
+% removes element from a list
 % remove(X, L, R).
 remove(_, [], []).
 remove(X, [X|L], L).
-
-remove(X, [X|L], [X|L1]) :- remove(X, )
+remove(Y, [X|L], [X|L1]) :- Y \= X, remove(Y, L, L1).
 
 
 % Selection Sort
-
 ssort([], []).
-ssort([M|S], L) :- list_min(M, L), remove(M, L, L1), ssort(S, L1).
+ssort([M|S], L) :- list_min(L, M), remove(M, L, L1), ssort(S, L1).
