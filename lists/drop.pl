354d31db99b2126560b46f78333fa91fe7dd7855
nshahpazov <nshahpazov@gmail.com>
2015-04-16 18:08:43 +0300

    fixed wrong param

--- lists/drop.pl
+++ lists/drop.pl
@@ -2,7 +2,7 @@
 % ?- drop([a, b, c, d, e, f, g, h, i, k], 3, X).
 % X = [a, b, d, e, g, h, k]
 
-drop_([], _, _, R).
+drop_([], _, _, []).
 drop_([H|T], N, K, [H|R]) :- K mod N =\= 0, K1 is K + 1,
   drop_(T, N, K1, R).
 drop_([_|T], N, K, R) :- K mod N =:= 0, K1 is K + 1,
