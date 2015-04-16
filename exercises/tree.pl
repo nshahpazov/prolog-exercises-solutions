91831e059f4842371372cb5909211ec802311858
nshahpazov <nshahpazov@gmail.com>
2015-04-16 18:08:30 +0300

    added add, tsort, ltr procedures

--- exercises/tree.pl
+++ exercises/tree.pl
@@ -1,35 +1,47 @@
-; tree in prolog
+% tree in prolog
 
-; Left, Root, Right
+% Left, Root, Right
 t(L, T, R).
 
-; tsort(S, L)
+% tsort(S, L)
 
-; make a tree from a list
-; maketree(L, T)
+% make a tree from a list
+% maketree(L, T)
 
-; ltr() ; traversing the tree
+% ltr() ; traversing the tree
 
 
-; maketree
-maketree([], e). ; making a tree from empty list is the constant e (empty)
+% maketree
+maketree([], e). % making a tree from empty list is the constant e (empty)
 maketree([H|L], T) :-
     maketree(L, T1),
     add(H, T1, T).
 
+add(X, e, t(e, X, e)).
 
-; structure of a tree
-; n + m + 1 = k
+add(X, t(L, T, R), t(L1, T, R)) :-
+    X < T, add(X, L, L1).
+
+ltr(e, []) :- ltr(L, SL),
+              ltr(R, SR),
+              append(SL, [T|SR], S)
+
+tsort(S, L) :- make(L, T), ltr(T, S).
+
+% structure of a tree
+% n + m + 1 = k
 t(K, [A,B]) :- K1 is K - 1,
     between(M, O, K1),
     N is K1 - M,
 
-    ; make the two trees
+    % make the two trees
     t(M, A),
     t(N, B).
 
+
+
+
 sorted([]).
 sorted([_]).
-sorted([A, B|T]) :-
-    less(A, B),
+sorted([A, B|T]) :- less(A, B),
     sorted([B|T]).
