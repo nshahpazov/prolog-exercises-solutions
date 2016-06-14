9ca140e6ccee359e9c85ccbd86cfe3e111b7d450
nshahpazov <nshahpazov@gmail.com>
2016-06-14 18:35:05 +0300

    P14 (*) Duplicate the elements of a list. Example: ?- dupli([a,b,c,c,d],X). X = [a,a,b,b,c,c,c,c,d,d]

--- lists/duplicate.pl
+++ lists/duplicate.pl
@@ -1,6 +1,9 @@
-duplicate_packed([], []).
-duplicate_packed([[E, T]|Es], [Z|Zs]) :- times(E, T * 2, Z),
-                                         duplicate_packed(Es, Zs).
+% duplicates elements in a list given an encoded list of pairs [El, Times]
+duplicate_encoded([], []).
+duplicate_encoded([[E, T]|Es], [Z|Zs]) :- times(E, T * 2, Z),
+                                         duplicate_encoded(Es, Zs).
 
+% encodes to [Element, Times] and then uses the duplicate_encoded to duplicate
+% the number of elements
 duplicate([], []).
-duplicate(Xs, Zs) :- encode(Xs, Ys), duplicate_packed(Ys, Zs).
+duplicate(Xs, Zs) :- encode(Xs, Ys), duplicate_encoded(Ys, Zs).
