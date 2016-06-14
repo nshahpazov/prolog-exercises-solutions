7a12a68ab7dd2ef1c27337b280fe1d8d7e3ca1b4
nshahpazov <nshahpazov@gmail.com>
2016-06-14 17:57:31 +0300

    Pack consecutive duplicates of list elements into sublists.

--- lists/pack.pl
+++ lists/pack.pl
@@ -8,9 +8,3 @@ pack([X, Y|T], [[X]|Rest]) :- X \= Y, pack([Y|T], Rest).
 % p99's solution
 pack_([], []).
 pack_([X|Xs], [Z|Zs]) :- transfer(X, Xs, Ys, Z), pack_(Ys, Zs).
-
-% transfer(X, Xs, Ys, Z) Ys is the list that remains from the list Xs
-%    when all leading copies of X are removed and transfered to Z
-transfer(X, [], [], [X]).
-transfer(X, [Y|Ys], [Y|Ys], [X]) :- X \= Y.
-transfer(X, [X|Xs], Ys, [X|Zs]) :- transfer(X, Xs, Ys, Zs).
