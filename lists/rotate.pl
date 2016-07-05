66a3a5c88a9bf70ecfaae7427614ed3b5210a9fe
nshahpazov <nshahpazov@gmail.com>
2016-07-05 19:14:41 +0300

    added another way of doing the rotate problem and renamed the exam problems

--- lists/rotate.pl
+++ lists/rotate.pl
@@ -1,6 +1,9 @@
 % rotate a list to the left (uses split procedure)
-% rotate([1,2,3,4,5], -2, R).
+% rotate([1, 2, 3, 4, 5], -2, R).
 % R = [4, 5, 1, 2, 3].
 rotate([], _, []).
 rotate(L, N, R) :- length(L, N2), N1 is N2 + N,
   append(Y, X, R), split(L, N1, X, Y).
+
+% another way of doing it is using split
+rotate(Xs, N, Rs) :- split(Xs, N, Ys, Zs), append(Zs, Ys, Rs).
--- lists/sample.pl
+++ lists/sample.pl
@@ -1,14 +1,5 @@
-
-%% slice([a, b, c, d, e, f, g, h, i, k], 3, 7, L).
-
-slice(_, 1, 0, []).
-slice([X|Xs], 1, K, [X|Rs]) :- K > 0,
-                               K1 is K - 1,
-                               slice(Xs, 1, K1, Rs).
-
-slice([_|Xs], N, K, Rs) :- N > 1,
-                           K > 0,
-                           N1 is N - 1,
-                           K1 is K - 1,
-                           slice(Xs, N1, K1, Rs).
-
+%rotate([a, b, c, d, e, f, g, h], 3, X).
+%X = [d, e, f, g, h, a, b, c]
+% use split
+rotate(Xs, N, Rs) :- split(Xs, N, Ys, Zs),
+                     append(Zs, Ys, Rs).
