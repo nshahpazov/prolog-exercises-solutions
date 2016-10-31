74b6b36e24f421809ca5f5b50404f4580e34e7a3
nshahpazov <nshahpazov@gmail.com>
2016-10-31 19:34:35 +0200

    added generators

--- labs.pl
+++ labs.pl
@@ -22,11 +22,11 @@ prefix(P, L) :- append(P, _, L).
 suffix(S, L) :- append(_, S, L).
 infix(I, L) :- append(P, _, L), append(_, I, P).
 
-add(X, L, [X|L]).
-add(X, [H|T], [H|N]) :- add(X, T, N).
+insert(X, L, [X|L]).
+insert(X, [H|T], [H|N]) :- insert(X, T, N).
 
 % removes X from L to get N
-remove(X, L, N) :- add(X, N, L).
+remove(X, L, N) :- insert(X, N, L).
 
 
 % permutation(P, L) - P is a permutation of L
@@ -50,6 +50,10 @@ permutation_sort(L, S) :- permutation(S, L), sorted(S).
 
 % second way using the existence quantifier technique
 is_sorted2(L) :- not((infix([A, B], L), not(less(B < A)))).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%% 19.10.2016 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % min2(X, A, B) :- X is A if A is less then B
 min_helper(A, A, B) :- less(A, B).
@@ -80,13 +84,59 @@ qs([H|T], R) :- split(T, H, R1, R2),
 % [L, T, R]
 
 add(X, [], [[], X, []]).
-add(X, [T1, Y, _]) :- X < Y, add(X, T1).
-add(X, [_, Y, T2]) :- not(X < Y), add(X, T2).
+add(X, [L1, Y, R], [L, Y, R]) :- X < Y, add(X, L1, L).
+add(X, [L, Y, R1], [L, Y, R]) :- not(X < Y), add(X, R1, R).
 
-maketree([H|T], [])
-maketree([H|T], R) :- add()
-
+maketree([], []).
 maketree([X|Xs], R) :- maketree(Xs, Y),
-                       add(H, Y, R).
+                       add(X, Y, R).
 
+ltr([L, T, R], A) :- ltr(L, Ls), ltr(R, Rs), append(Ls, [T|Rs], A).
 tsort(L, S) :- maketree(L, T), ltr(T, S).
+
+reverse([], []).
+reverse([H|T], R) :- reverse(T, T1), append(T1, [H], R).
+
+% reverse using the stack technique
+reverse_([], S, S).
+reverse_([H|L], S, R) :- reverse_(L, [H|S], R).
+
+reverse2(L, R) :- reverse_(L, [], R).
+
+% arithmetics
+size([], 0).
+size([_|T], N) :- size(T, N1), N is N1 + 1.
+
+sum([], 0).
+sum([H|T], S) :- sum(T, S1), S is S1 + H.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%% 26.10.2016 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+nth([H|_], 0, H).
+nth([_|T], N, X) :- N1 is N - 1, nth(T, N1, X).
+
+% set operations generators
+intersection_generator(A, B, X) :- member(X, A), member(X, B).
+
+union_generator(A, _, X) :- member(X, A).
+union_generator(_, B, X) :- member(X, B).
+
+difference_generator(A, B, X) :- member(X, A), not(member(X, B)).
+
+% number generators
+natural(0).
+natural(N) :- natural(N1), N is N1 + 1.
+
+odd(1).
+odd(N) :- odd(N1), N is N1 + 2.
+
+even(0).
+even(X) :-  even(X1), X is X1 + 2.
+
+fibonacci_(0, 1).
+fibonacci_(Y, Z) :- fibonacci_(X, Y), Z is X + Y.
+fibonacci(N) :- fibonacci_(N, _).
+
+% todo: must understand reverse with stack, subsets, and this crazy fibonacci thing
